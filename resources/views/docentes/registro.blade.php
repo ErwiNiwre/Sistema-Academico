81a18cf92ec3287a55425fc262b1fe75bb8b9f69
“ErwiNiwre” <niwre7690@gmail.com>
2018-09-29 18:35:12 -0400

    Vistas de Docentes

--- resources/views/docentes/registro.blade.php
+++ resources/views/docentes/registro.blade.php
@@ -1,10 +1,11 @@
 <div class="box-header with-border">
-    <h3 class="box-title">Datosss</h3>
+    <h3 class="box-title">Datos</h3>
 </div>
 <form class="form-horizontal">
     <div class="box-body">
+        <label class="control-label">Item:</label>
         <div class="form-group">
-            <label class="col-sm-2 control-label">Item:</label>
+            <label class="col-sm-2 control-label"></label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="inputEmail3" placeholder="Carnet de Identidad">
             </div>
@@ -12,36 +13,49 @@
         <label class="control-label">Cedula Identidad:</label>
         <div class="form-group">
             <label class="col-sm-2 control-label"></label>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" id="inputPassword3" placeholder="Password">
+            </div>
+            <div class="col-sm-4">
+                {{--  {{ $departamento }}  --}}
+                <select name="example1_length" aria-controls="example1" class="form-control input-sm">
+                  @foreach ($departamento as $departamentos)
+                    <option value="{{ $departamentos['id']}}">{{ $departamentos['abreviatura']}}</option>    
+                  @endforeach
+                </select> 
+            </div>
+        </div>
+        <label class="control-label">Paterno:</label>
+        <div class="form-group">
+            <label class="col-sm-2 control-label"></label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="inputPassword3" placeholder="Password">
             </div>
         </div>
+        <label class="control-label">Materno:</label>
         <div class="form-group">
-            <label class="col-sm-2 control-label">Paterno:</label>
+            <label class="col-sm-2 control-label"></label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="inputPassword3" placeholder="Password">
             </div>
         </div>
+        <label class="control-label">Nombres:</label>
         <div class="form-group">
-            <label class="col-sm-2 control-label">Materno:</label>
+            <label class="col-sm-2 control-label"></label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="inputPassword3" placeholder="Password">
             </div>
         </div>
+        <label class="control-label">Fecha de Nacimiento:</label>
         <div class="form-group">
-            <label class="col-sm-2 control-label">Nombres:</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="inputPassword3" placeholder="Password">
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-sm-2 control-label">Fecha de Nacimiento:</label>
+            <label class="col-sm-2 control-label"></label>
             <div class="col-sm-10">
                 <input type="date" class="form-control" id="inputPassword3" placeholder="Password">
             </div>
         </div>
+        <label class="control-label">Genero:</label>
         <div class="form-group">
-            <label class="col-sm-2 control-label">Genero:</label>
+            <label class="col-sm-2 control-label"></label>
             <div class="col-sm-10">
                 <select class="form-control" placeholder="dd">
                     <option value="">Seleccione una Opción</option>
@@ -50,26 +64,30 @@
                 </select> 
             </div>
         </div>
+        <label class="control-label">Correo Electronico:</label>
         <div class="form-group">
-            <label class="col-sm-2 control-label">Correo Electronico:</label>
+            <label class="col-sm-2 control-label"></label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="inputPassword3" placeholder="Password">
             </div>
         </div>
+        <label class="control-label">Dirección:</label>
         <div class="form-group">
-            <label class="col-sm-2 control-label">Dirección:</label>
+            <label class="col-sm-2 control-label"></label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="inputPassword3" placeholder="Password">
             </div>
         </div>
+        <label class="control-label">Telefono:</label>
         <div class="form-group">
-            <label class="col-sm-2 control-label">Telefono:</label>
+            <label class="col-sm-2 control-label"></label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="inputPassword3" placeholder="Password">
             </div>
         </div>
+        <label class="control-label">Celular:</label>
         <div class="form-group">
-            <label class="col-sm-2 control-label">Celular:</label>
+            <label class="col-sm-2 control-label"></label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="inputPassword3" placeholder="Password">
             </div>
---
+++ resources/views/usuarios/registro.blade.php
@@ -0,0 +1,31 @@
+<div class="box-header with-border">
+    <h3 class="box-title">Usuario</h3>
+</div>
+<form class="form-horizontal">
+    <div class="box-body">
+        <div class="form-group">
+            <label for="inputEmail3" class="col-sm-2 control-label">Usuario:</label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Carnet de Identidad">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="inputPassword3" class="col-sm-2 control-label">Password:</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox"> Remember me
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="box-footer">
+        <button type="submit" class="btn btn-info pull-right">Guardar</button>
+    </div>
+</form>
